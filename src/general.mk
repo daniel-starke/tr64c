59f52cbe61d8c5f67a848dc26fd5e98194c4614c
daniel-starke <daniel-email@gmx.net>
2018-08-14 22:31:47 +0200

    fixed undefined putenv

--- src/general.mk
+++ src/general.mk
@@ -1,5 +1,5 @@
 CWFLAGS = -Wall -Wextra -Wformat -pedantic -Wshadow -Wno-format -std=c99
-CFLAGS = -O2 -DNDEBUG -D_BSD_SOURCE -D_POSIX_C_SOURCE=200112L -mtune=core2 -march=core2 -mstackrealign -fomit-frame-pointer -fno-ident
+CFLAGS = -O2 -DNDEBUG -D_BSD_SOURCE -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE -mtune=core2 -march=core2 -mstackrealign -fomit-frame-pointer -fno-ident
 LDFLAGS = -s -fno-ident
 PATHS = 
 LIBS = 
--- src/linux.mk
+++ src/linux.mk
@@ -1,5 +1,5 @@
 CWFLAGS = -Wall -Wextra -Wformat -pedantic -Wshadow -Wno-format -std=c99
-CFLAGS = -O2 -DNDEBUG -D_BSD_SOURCE -D_POSIX_C_SOURCE=200112L -mstackrealign -fno-ident -D_LARGEFILE64_SOURCE
+CFLAGS = -O2 -DNDEBUG -D_BSD_SOURCE -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE -mstackrealign -fno-ident -D_LARGEFILE64_SOURCE
 LDFLAGS = -s -fno-ident
 PATHS = 
 LIBS = 
